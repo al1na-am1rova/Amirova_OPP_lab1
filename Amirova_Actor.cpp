687bec457805939cbcd6c5782ce7312708adf26f
al1na-am1rova <li.amirova2000@mail.ru>
2024-09-04 19:33:17 +0300

    Добавьте файлы проекта.

---
+++ Amirova_Actor.cpp
@@ -0,0 +1,7 @@
+#include "Amirova_Actor.h"
+
+using namespace std;
+
+//cout << "Name: ";
+//cin.ignore(1000, '\n');
+//getline(cin, new_pipe.name);
---
+++ Amirova_Actor.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Amirova_Actor
+{
+public:
+	string name;
+	int age;
+	double height; 
+	bool is_able_to_sing;
+	string city;
+	
+};
+
